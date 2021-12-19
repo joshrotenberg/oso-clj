db43300b50275d74d4260a38079dd0ea553949cd
josh rotenberg <joshrotenberg@users.noreply.github.com>
2021-12-19 14:46:18 -0800

    add some defrecord

--- src/joshrotenberg/oso.clj
+++ src/joshrotenberg/oso.clj
@@ -1,4 +1,8 @@
 (ns joshrotenberg.oso
+  "This is a sort of stream of consciousness Clojure file to see
+  what a wrapper of the Oso Java library might look, starting out with
+  just basic interop and attempting to come up with something a bit more
+  idiomatic if possible."
   (:require [clojure.java.io :as io])
   (:import [com.osohq.oso Oso]
            [java.util Map]))
@@ -50,6 +54,38 @@
       (load-files [path]))
     (let [user {"name" "president"}
           company {"id" 1}]
-      (.isAllowed oso user "create" company)))
-;;
-  )
+      (.isAllowed oso user "create" company))))
+
+;; so obviously we need to register classes that can fulfill the contract 
+;; we've told oso about. we can get close in clojure with defprotocol/defrecord
+
+(declare joshrotenberg.oso.User)
+(declare joshrotenberg.oso.Widget)
+(declare joshrotenberg.oso.Company)
+
+(defprotocol CompaniesProtocol (companies [_]))
+(defprotocol RoleProtocol (role [this user]))
+
+(defrecord User [name] CompaniesProtocol (companies [this] (Company. 1)))
+(defrecord Widget [id])
+(defrecord Company [id]
+  RoleProtocol (role [this user] (if (= (:name user) "president")
+                                   "admin"
+                                   "guest")))
+(comment
+  ;; unfortunately this doesn't work. the record has the methods but its also a map
+  ;; and we get this error because the map keys are keywords and not strings:
+  ;; Execution error (Exceptions$UnexpectedPolarTypeError) at com.osohq.oso.Host/toPolarTerm (Host.java:234).
+  ;; Cannot convert map with non-string keys to Polar
+  (let [oso (new-oso)
+        path (-> "test_oso.polar"
+                 io/resource
+                 io/file
+                 .getPath)
+        user (User. "president")
+        company (Company. 1)]
+    (doto oso
+      (register-class joshrotenberg.oso.User "User")
+      (register-class joshrotenberg.oso.Company "Company")
+      (register-class joshrotenberg.oso.Widget "Widget"))
+    (.isAllowed oso user "create" company)))
